6ce8b3d0618248ad784524d90265950f61badd7b
chandradev <Chandradev_Prasad@Dellteam.com>
2020-10-10 19:23:21 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DataMigration.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
